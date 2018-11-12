893b3eaa81ce2d25a39f698e2b90c7a20a2bc7da
naz585 <oldskool11218@gmail.com>
2018-11-11 23:07:14 -0500

    Update app.js

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ $(document).ready(function() {
     $(this).addClass("active");
 
     var type = $(this).attr("data-type");
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + type + "&api_key=ChZ7QEoRoWNeksqTD2HgL2a628hVNo7v&limit=10";
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + type + "&api_key=ChZ7QEoRoWNeksqTD2HgL2a628hVNo7v&limit=10";
 
     $.ajax({
       url: queryURL,
